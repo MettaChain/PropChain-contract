a662eb2701f336e0f3fdef62d1eee1fba8cc5eae
LaGodxy <LaGodxy@users.noreply.github.com>
2026-02-21 13:00:29 +0000

    deploy: eb120c3009f80b5aba83404acb8602eecce1e727

---
+++ trait.impl/propchain_traits/trait.Oracle.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["oracle",[["impl Oracle for <a class=\"struct\" href=\"oracle/struct.PropertyValuationOracle.html\" title=\"struct oracle::PropertyValuationOracle\">PropertyValuationOracle</a>"]]],["propchain_contracts",[]],["propchain_traits",[]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[181,27,24]}
