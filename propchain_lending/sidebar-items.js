c9a6807d43865bd3f52c7bf8ef29de428862aadd
LaGodxy <LaGodxy@users.noreply.github.com>
2026-03-29 22:51:42 +0000

    deploy: 77a83151c69f7045150841f87b917e3405480ffb

---
+++ propchain_lending/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["LendingError"],"struct":["PropertyLending"]};
--- trait.impl/ink_storage_traits/storage/trait.StorableHint.js
+++ trait.impl/ink_storage_traits/storage/trait.StorableHint.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["propchain_insurance",[["impl&lt;__ink_generic_salt: StorageKey&gt; StorableHint&lt;__ink_generic_salt&gt; for <a class=\"struct\" href=\"propchain_insurance/struct.PropertyInsurance.html\" title=\"struct propchain_insurance::PropertyInsurance\">PropertyInsurance</a>",0]]],["propchain_traits",[["impl&lt;__ink_generic_salt: StorageKey&gt; StorableHint&lt;__ink_generic_salt&gt; for <a class=\"struct\" href=\"propchain_traits/access_control/struct.AccessControl.html\" title=\"struct propchain_traits::access_control::AccessControl\">AccessControl</a>",0]]]]);
+    const implementors = Object.fromEntries([["propchain_insurance",[["impl&lt;__ink_generic_salt: StorageKey&gt; StorableHint&lt;__ink_generic_salt&gt; for <a class=\"struct\" href=\"propchain_insurance/struct.PropertyInsurance.html\" title=\"struct propchain_insurance::PropertyInsurance\">PropertyInsurance</a>",0]]],["propchain_lending",[["impl&lt;__ink_generic_salt: StorageKey&gt; StorableHint&lt;__ink_generic_salt&gt; for <a class=\"struct\" href=\"propchain_lending/struct.PropertyLending.html\" title=\"struct propchain_lending::PropertyLending\">PropertyLending</a>",0]]],["propchain_traits",[["impl&lt;__ink_generic_salt: StorageKey&gt; StorableHint&lt;__ink_generic_salt&gt; for <a class=\"struct\" href=\"propchain_traits/access_control/struct.AccessControl.html\" title=\"struct propchain_traits::access_control::AccessControl\">AccessControl</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[274,285]}
+//{"start":59,"fragment_lengths":[274,263,285]}
